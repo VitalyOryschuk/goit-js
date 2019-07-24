addf93c6c16e7baac8ae927e4919f0b49cb0d0be
Vitaly Oryschuk <Leilyua@ukr.net>
2019-07-24 21:13:49 +0300

    if else

--- classworks/01/js/app.js
+++ classworks/01/js/app.js
@@ -10,36 +10,73 @@
 // dog = 'Bob';
 // cat = 'Tomas';
 
-let password, salary;
-const userName = 'Bob';
-const userLastName = 'Dilan';
-const age = '55';
-let isLogedin = false;
-const VAT = 0.2;
+// let password, salary;
+// const userName = 'Bob';
+// const userLastName = 'Dilan';
+// const age = '55';
+// let isLogedin = false;
+// const VAT = 0.2;
 
+// console.log(userName, userLastName);
+// // alert(userName);
+// // NaN
+// console.log(userName / age);
+// console.log(typeof userName);
 
-console.log(userName, userLastName);
-// alert(userName);
-// NaN
-console.log(userName / age);
-console.log(typeof userName);
+// isLogedin = confirm('Are you loggedin?');
 
-isLogedin = confirm('Are you loggedin?');
+// if (isLogedin) {
+//   const salaryWithoutVAT = prompt('Enter you salary');
+//   salary = +salaryWithoutVAT * (1 - VAT);
+//   console.log(salary);
+// }
+// const foo = 5 % 3;
+// let isEven = foo === 0;
+// console.log(isEven);
+// const result = `Hello ${userName} ${userLastName}!!
+//  Your age is ${age}. Your salary ${salary}`;
+// alert(result);
 
-if (isLogedin) {
-  const salaryWithoutVAT = prompt('Enter you salary');
-  salary = +salaryWithoutVAT * (1 - VAT);
-  console.log(salary);
-}
-const foo = 5 % 3;
-let isEven = foo === 0;
-console.log(isEven);
-const result = `Hello ${userName} ${userLastName}!!
- Your age is ${age}. Your salary ${salary}`;
-alert(result);
+// if (userName && userLastName && salary) {
+//   console.log('is valid');
+// } else if (userName || userLastName || salary) {
+//   console.log('somesing fullfilled');
+// }
+// const foo = 5;
 
-if (userName && userLastName && salary) {
-  console.log('is valid')
-} else if (userName || userLastName || salary) {
-  console.log('somesing fullfilled')
+// if (foo > 10) {
+// } else if (foo === 9) {
+// } else {
+//   console.log(foo);
+// }
+
+// let tax;
+// const age = 20;
+
+// tax = age > 50 ? 10 : 5;
+// if (age > 50 ){
+//   tax = 10
+// }else {
+//   tax = 5;
+// }
+// let message;
+// const age = prompt('enter your age');
+
+// if (age >= 10 && age <= 20) {
+//   message = 'child';
+// } else if (age > 20 && age <= 50) {
+//   message = 'adult';
+// } else if (age > 50) {
+//   message = 'granny';
+// }
+// alert(message);
+
+// do {
+//   console.log(`apple=${apple}`);
+// } while (apple > 200);
+// {
+//   console.log(error);
+// }
+for (let index = 0; index < apple; index++) {
+  console.log(`index=${index}`);
 }
