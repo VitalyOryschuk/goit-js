02186dcf877c4920fe75e93891b4dac1d59ee2d9
Vitaly Oryschuk <Leilyua@ukr.net>
2019-07-27 20:43:40 +0300

    add task-06

--- homeworks/homework-01/js/task-01.js
+++ homeworks/homework-01/js/task-01.js
@@ -1,9 +1,9 @@
-// 'use strict';
-// const name = 'Генератор защитного поля';
-// let price;
-// price = 1000;
-// let product = `Выбран ${name}, цена за штуку ${price} кредитов`;
-// console.log(product);
-// price = 2000;
-// product = `Выбран ${name}, цена за штуку ${price} кредитов`;
-// console.log(product);
+'use strict';
+const name = 'Генератор защитного поля';
+let price;
+price = 1000;
+let product = `Выбран ${name}, цена за штуку ${price} кредитов`;
+console.log(product);
+price = 2000;
+product = `Выбран ${name}, цена за штуку ${price} кредитов`;
+console.log(product);
--- homeworks/homework-01/js/task-02.js
+++ homeworks/homework-01/js/task-02.js
@@ -1,14 +1,14 @@
-// const total = 100;
-// let message;
-// const ordered = prompt('Введите количество товара');
-// if (ordered <= total) {
-//   message = 'Заказ оформлен, с вами свяжется менеджер';
+const total = 100;
+let message;
+const ordered = prompt('Введите количество товара');
+if (ordered <= total) {
+  message = 'Заказ оформлен, с вами свяжется менеджер';
 
-//   if (ordered <= 0) {
-//     message = 'Ошибка!';
-//   }
-// } else {
-//   message = 'На складе недостаточно товаров!';
-// }
+  if (ordered <= 0) {
+    message = 'Ошибка!';
+  }
+} else {
+  message = 'На складе недостаточно товаров!';
+}
 
-// console.log(message);
+console.log(message);
