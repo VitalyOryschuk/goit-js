0cbb58c09b961803379115abaa360b270fbf9aae
Vitaly Oryschuk <Leilyua@ukr.net>
2019-07-30 12:24:36 +0300

    all set

--- docs/homeworks/homework-01/js/task02.js
+++ docs/homeworks/homework-01/js/task02.js
@@ -1,7 +1,6 @@
 const total = 100;
 let ordered = 50;
 let inputOrder = prompt('Введите количество товара');
-}
 if (inputOrder > total && ordered === true) {
   console.log('На складе недостаточно товаров!');
 } else {
