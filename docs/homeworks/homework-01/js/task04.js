e41032431d561f40a85682df69370fed24e19fe3
Vitaly Oryschuk <Leilyua@ukr.net>
2019-07-30 12:11:05 +0300

    resubmit task04

--- docs/homeworks/homework-01/js/task04.js
+++ docs/homeworks/homework-01/js/task04.js
@@ -1,17 +1,13 @@
-// let credits = 23580;
-// const pricePerDroid = 3000;
-// let totalPrice;
-// // let message;
-// let buyDroids = prompt('Сколько дроидов вы хотите купить?');
-// totalPrice = buyDroids * pricePerDroid;
-// if (buyDroids === null) {
-//   totalPrice = 'Отменено пользователем!';
-//   console.log(totalPrice);
-// }
-// if (totalPrice > credits) {
-//   totalPrice = 'Недостаточно средств на счету!';
-// } else {
-//   totalPrice = `Вы купили ${buyDroids} дроидов, на счету осталось ${credits - totalPrice} кредитов`;
-//   console.log(totalPrice);
-//   // console.log(message);
-// }
+let credits = 23580;
+const pricePerDroid = 3000;
+let totalPrice;
+let buyDroids = prompt('Сколько дроидов вы хотите купить?');
+if (buyDroids === null) {
+  console.log('Отменено пользователем!');
+}
+totalPrice = buyDroids * pricePerDroid;
+if (totalPrice > credits) {
+  console.log('Недостаточно средств на счету!');
+} else {
+  console.log(`Вы купили ${buyDroids} дроидов, на счету осталось ${credits - totalPrice} кредитов`);
+}
