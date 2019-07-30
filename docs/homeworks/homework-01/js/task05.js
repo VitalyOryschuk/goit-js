70b108466f77ece53e37ea74f251f22e7857c004
Vitaly Oryschuk <Leilyua@ukr.net>
2019-07-30 12:14:43 +0300

    resubmit task06

--- docs/homeworks/homework-01/js/task05.js
+++ docs/homeworks/homework-01/js/task05.js
@@ -1,30 +1,30 @@
-// let costofDelivery;
-// let userChoise = prompt('Выберите страну доставки');
-// if (userChoise === null) {
-//   alert('Отмена');
-// } else {
-//   switch (userChoise.toLowerCase()) {
-//     case 'китай':
-//       costofDelivery = 100;
-//       alert(`Доставка в Китай будет стоить ${costofDelivery} кредитов`);
-//       break;
-//     case 'чили':
-//       costofDelivery = 250;
-//       alert(`Доставка в Чили будет стоить ${costofDelivery} кредитов`);
-//       break;
-//     case 'украина':
-//       costofDelivery = 80;
-//       alert(`Доставка в Украину будет стоить ${costofDelivery} кредитов`);
-//       break;
-//     case 'австралия':
-//       costofDelivery = 170;
-//       alert(`Доставка в Австралия будет стоить ${costofDelivery} кредитов`);
-//       break;
-//     case 'ямайка':
-//       costofDelivery = 120;
-//       alert(`Доставка в Ямайку будет стоить ${costofDelivery} кредитов`);
-//       break;
-//     default:
-//       alert('В вашей стране доставка не доступна');
-//   }
-// }
+let costofDelivery;
+let userChoise = prompt('Выберите страну доставки');
+if (userChoise === null) {
+  alert('Отмена');
+} else {
+  switch (userChoise.toLowerCase()) {
+    case 'китай':
+      costofDelivery = 100;
+      alert(`Доставка в Китай будет стоить ${costofDelivery} кредитов`);
+      break;
+    case 'чили':
+      costofDelivery = 250;
+      alert(`Доставка в Чили будет стоить ${costofDelivery} кредитов`);
+      break;
+    case 'украина':
+      costofDelivery = 80;
+      alert(`Доставка в Украину будет стоить ${costofDelivery} кредитов`);
+      break;
+    case 'австралия':
+      costofDelivery = 170;
+      alert(`Доставка в Австралия будет стоить ${costofDelivery} кредитов`);
+      break;
+    case 'ямайка':
+      costofDelivery = 120;
+      alert(`Доставка в Ямайку будет стоить ${costofDelivery} кредитов`);
+      break;
+    default:
+      alert('В вашей стране доставка не доступна');
+  }
+}
--- docs/homeworks/homework-01/js/task06.js
+++ docs/homeworks/homework-01/js/task06.js
@@ -1,12 +1,12 @@
-// let input;
-// let total = 0;
-// for (let i = 0; i !== null; i++) {
-//   input = prompt('Введите число');
-//   if (input === null) {
-//     total = `Общая сумма чисел равна ${total}`;
-//     break;
-//   }
-//   input = Number(input);
-//   total += input;
-// }
-// alert(total);
+let input;
+let total = 0;
+for (let i = 0; i !== null; i++) {
+  input = prompt('Введите число');
+  if (input === null) {
+    total = `Общая сумма чисел равна ${total}`;
+    break;
+  }
+  input = Number(input);
+  total += input;
+}
+alert(total);
