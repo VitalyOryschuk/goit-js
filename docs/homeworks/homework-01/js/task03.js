0991e86250e23dea84ebc7626ee9eb0c5e4bab07
Vitaly Oryschuk <Leilyua@ukr.net>
2019-07-30 12:23:48 +0300

    all set

--- docs/homeworks/homework-01/js/task03.js
+++ docs/homeworks/homework-01/js/task03.js
@@ -1,12 +1,12 @@
-// const ADMIN_PASSWORD = 'jqueryismyjam';
-// let message;
-// const userPassword = prompt('Введите пароль');
-// if (userPassword !== ADMIN_PASSWORD) {
-//   message = 'Доступ запрещен, неверный пароль!';
-//   if (userPassword === null) {
-//     message = 'Отменено пользователем!';
-//   }
-// } else {
-//   message = 'Добро пожаловать!';
-// }
-// alert(message);
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message;
+const userPassword = prompt('Введите пароль');
+if (userPassword !== ADMIN_PASSWORD) {
+  message = 'Доступ запрещен, неверный пароль!';
+  if (userPassword === null) {
+    message = 'Отменено пользователем!';
+  }
+} else {
+  message = 'Добро пожаловать!';
+}
+alert(message);
