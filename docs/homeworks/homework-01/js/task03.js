1eaa9e4bf95cd4becbf8c5194fff4ae06561ec4a
Vitaly Oryschuk <Leilyua@ukr.net>
2019-08-01 15:13:36 +0300

    resubmit task3

--- docs/homeworks/homework-01/js/task03.js
+++ docs/homeworks/homework-01/js/task03.js
@@ -1,12 +1,13 @@
 const ADMIN_PASSWORD = 'jqueryismyjam';
 let message;
 const userPassword = prompt('Введите пароль');
-if (userPassword !== ADMIN_PASSWORD) {
-  message = 'Доступ запрещен, неверный пароль!';
-  if (userPassword === null) {
-    message = 'Отменено пользователем!';
-  }
-} else {
+if (userPassword === null) {
+  message = 'Отменено пользователем!';
+}
+if (userPassword === ADMIN_PASSWORD) {
   message = 'Добро пожаловать!';
+
+} else {
+  message = 'Доступ запрещен, неверный пароль!';
 }
 alert(message);
