8dd25e8258bb04061d6e2e31919ac96d20b67a02
Vitaly Oryschuk <Leilyua@ukr.net>
2019-08-01 15:10:55 +0300

    resubmit task2

--- docs/homeworks/homework-01/js/task02.js
+++ docs/homeworks/homework-01/js/task02.js
@@ -1,7 +1,6 @@
 const total = 100;
 let ordered = 50;
-let inputOrder = prompt('Введите количество товара');
-if (inputOrder > total && ordered === true) {
+if (ordered > total) {
   console.log('На складе недостаточно товаров!');
 } else {
   console.log('Заказ оформлен, с вами свяжется менеджер');
