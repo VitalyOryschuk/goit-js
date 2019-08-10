8c06c0214ecbec3f9fba5f961b2ac83a2d4dd1d9
Vitaly Oryschuk <Leilyua@ukr.net>
2019-08-10 11:44:14 +0300

    added task04

---
+++ docs/homeworks/homework-03/js/task-03.js
@@ -0,0 +1,36 @@
+// const findBestEmployee = function(employees) {
+//   // let arr = Object.entries(employees);
+//   // Math.max(arr);
+//   // return arr;
+
+//   for (let key in keys) {
+//     // value = Math.max(value);
+//     // let values = Object.values(employees);
+//     return employees[key];
+//   }
+// };
+// console.log(
+//   findBestEmployee({
+//     ann: 29,
+//     david: 35,
+//     helen: 1,
+//     lorence: 99,
+//   }),
+// );
+
+// console.log(
+//   findBestEmployee({
+//     poly: 12,
+//     mango: 17,
+//     ajax: 4,
+//   }),
+// );
+
+// console.log(
+//   findBestEmployee({
+//     lux: 147,
+//     david: 21,
+//     kiwi: 19,
+//     chelsy: 38,
+//   }),
+// );
