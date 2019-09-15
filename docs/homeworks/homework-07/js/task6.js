f80c51aab948170b6f7f2b469e65da0510692146
Vitaly Oryschuk <Leilyua@ukr.net>
2019-09-15 22:11:55 +0300

    added task5-6

--- docs/homeworks/homework-07/js/task6.js
+++ docs/homeworks/homework-07/js/task6.js
@@ -1,8 +1,9 @@
 'use strict'
 const input = document.querySelector('#validation-input');
+const inputIsValid = Number(input.getAttribute('data-length'));
 input.addEventListener('change', event => {
   input.classList.add('invalid');
-  if (event.currentTarget.value.length === 6) {
+  if (event.currentTarget.value.length === inputIsValid) {
     input.classList.replace('invalid', 'valid');
   } else {
     input.classList.replace('valid', 'invalid');
