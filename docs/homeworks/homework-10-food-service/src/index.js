c8137b9c0c9bfd0397566ada0379f92107190c3f
Vitaly Oryschuk <Leilyua@ukr.net>
2019-10-08 21:47:22 +0300

    all set

--- docs/homeworks/homework-10-food-service/src/index.js
+++ docs/homeworks/homework-10-food-service/src/index.js
@@ -1,18 +1,39 @@
 import './styles.css';
 import menu from './menu.json';
-import menuItem from './templates/menu-item.hbs';
+import menuItem from './menu-item.hbs';
 
-console.log(menu);
-const refs = {
-  menu: document.querySelector('.menu'),
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
 };
-// const markup = menuItem(menu[0]);
-// console.log(markup);
+const Refs = {
+  menu: document.querySelector('.menu'),
+  bodyTheme: document.querySelector('body'),
+  input: document.querySelector('#theme-switch-control'),
+};
 
+Refs.input.addEventListener('change', changeTheme);
+
+function changeTheme() {
+  Refs.bodyTheme.classList.toggle(Theme.DARK);
+  if (Refs.bodyTheme.classList.contains(Theme.DARK)) {
+    Refs.bodyTheme.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    Refs.bodyTheme.classList.toggle(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+};
+
+if (localStorage.getItem('theme') === Theme.DARK) {
+  Refs.input.setAttribute('checked', true);
+  Refs.bodyTheme.classList.toggle(Theme.DARK);
+};
+
+// console.log(markup)
 buildMenu(menu);
 
 function buildMenu(menu) {
   const markup = menu.map(item => menuItem(item)).join('');
-  refs.menu.insertAdjacentHTML('beforeend', markup);
-  console.log(markup);
-}
+  Refs.menu.insertAdjacentHTML('beforeend', markup);
+};
