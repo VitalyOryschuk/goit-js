26418287ddaf84c1078e7132855f904e3be14f4e
Vitaly Oryschuk <Leilyua@ukr.net>
2019-11-27 17:53:11 +0200

    added html

---
+++ docs/classworks/resume/build/main.97bd3a56410402e48335.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{L1EO:function(n,o,i){},QfWi:function(n,o,i){"use strict";i.r(o);i("L1EO"),i("oe7G")},oe7G:function(n,o,i){}},[["QfWi",1]]]);
+//# sourceMappingURL=main.97bd3a56410402e48335.js.map
